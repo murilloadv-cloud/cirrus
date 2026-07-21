e432f1807e2a7fda7a8221f06805b303d9f59173
murilloadv-cloud <murilloadv@gmail.com>
2026-07-21 18:08:48 -0300

    fix supabase env vars

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
- 
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://zpdokuxxerzrnnxillpi.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_fNjywf4-9lV56T2CojEMCw_TMsmvJZ2'
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL || 'https://zpdokuxxerzrnnxillpi.supabase.co'
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY || 'sb_publishable_fNjywf4-9lV56T2CojEMCw_TMsmvJZ2'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
